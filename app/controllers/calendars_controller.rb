106e49f57868f8afbf74ddfde36e31efa8e65948
atsushi0610 <kogure.bbc@gmail.com>
2023-02-16 18:33:52 +0900

    メソッド命名修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,13 +2,13 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getWeek 
+    get_week 
     @plan = Plan.new
   end
 
   # 予定の保存
   def create
-    Plan.create(plan_params) #プライベートメソッドのplan_paramsからplanモデルの:dateと:planを受け取り保存する。
+    Plan.create(plan_params)
     redirect_to action: :index
   end
 
@@ -18,7 +18,7 @@ class CalendarsController < ApplicationController
     params.require(:plan).permit(:date, :plan) 
   end
 
-  def getWeek
+  def get_week
 
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
